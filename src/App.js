03e1674b0673c02ff11d439b5a5d7b307dcff0af
Eugene-Shcherbak <yevhen.shcherbak.kn.2021@lpnu.ua>
2022-07-15 17:10:24 +0300

    Сreated user side general layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
-import Header from './components/Header.js';
-import LeftVerticalMenu from './components/LeftVerticalMenu.js';
-import ContentArea from './components/ContentArea';
+import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
+import LeftSideBar from './components/LeftSideBar/LeftSideBar.js';
+import RightSideBar from "./components/RightSideBar/RightSideBar";
+import MainContentArea from "./components/MainContentArea/MainContentArea";
 
 export default function App() {
   return (
     <div>
-      <Header />
-      <LeftVerticalMenu />
-      <ContentArea />
+        <Header />
+        <LeftSideBar />
+        <RightSideBar />
+        <MainContentArea />
+        <Footer />
     </div>
   );
 }
