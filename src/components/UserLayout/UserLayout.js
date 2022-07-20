e17e21a2f84d3322072689baa63b60cd21cde754
Yurii-Hubych <yurii.hubych.pm.2020@lpnu.ua>
2022-07-20 22:27:00 +0300

    removed unused items

--- src/components/UserLayout/UserLayout.js
+++ src/components/UserLayout/UserLayout.js
@@ -1,11 +1,6 @@
 import React from "react";
 import UserHeader from "../Header/UserHeader"
 import LeftSideBar from '../LeftSideBar/LeftSideBar.js';
-import Footer from "../Footer/Footer";
-import RightSideBar from "../RightSideBar/RightSideBar";
-import PrimaryContentArea from "../PrimaryContentArea/PrimaryContentArea";
-import SecondaryContentArea from "../SecondaryContentArea/SecondaryContentArea";
-import AdditionalContentArea from "../AdditionalContentArea/AdditionalContentArea";
 import {Outlet} from "react-router-dom";
 
 export default function UserLayout(){
