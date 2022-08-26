027b48888a8ec427117d2ee0e5a537c31e5a0138
Juli11V <Juli11V>
2022-08-26 17:23:28 +0300

    fixed warning

--- src/components/AdminContentArea/AdminContentArea.js
+++ src/components/AdminContentArea/AdminContentArea.js
@@ -6,6 +6,7 @@ import AdminSearchContentArea from "./AdminSearchContentArea/AdminSearchContentA
 
 const articles = [
     {
+        Id: 'd95a7a26-eb68-4af2-907e-0cbedd40daf7',
         ArticleImage: 'https://miro.medium.com/max/700/0*svYfJQz2cmcnG9nO',
         HeadLine: "As M.L.B.’s Season Opens in Japan, at Least the Dirt Is Familiar",
         Content: "TOKYO — Major League Baseball begins its 2019 season on Wednesday in Japan with the first of two games between the Oakland Athletics and the Seattle Mariners. But when the teams ",
@@ -13,7 +14,7 @@ const articles = [
         Team:"Tennessee",
         Status:"Published"
     },
-    {
+    {   Id: '88666160-6a39-4a4d-8839-7874472ad412',
         ArticleImage: 'https://miro.medium.com/max/700/0*svYfJQz2cmcnG9nO',
         HeadLine: "2 As M.L.B.’s Season Opens in Japan, at Least the Dirt Is Familiar",
         Content: "2 TOKYO — Major League Baseball begins its 2019 season on Wednesday in Japan with the first of two games between the Oakland Athletics and the Seattle Mariners. But when the teams ",
@@ -21,7 +22,7 @@ const articles = [
         Team:"Tennessee2",
         Status:"Unpublished"
     },
-    {
+    {   Id: '6c308c3c-0c97-4177-8a4d-31881cbe80f8',
         ArticleImage: 'https://miro.medium.com/max/700/0*svYfJQz2cmcnG9nO',
         HeadLine: "headline article3",
         Content: "some text3",
