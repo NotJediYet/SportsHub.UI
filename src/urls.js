cd2e29b44331934541601ea3aad07a9f0525c6e5
foxerest <kuliantaras@gmail.com>
2022-09-01 21:35:43 +0300

    add local server url

--- src/urls.js
+++ src/urls.js
@@ -1,7 +1,7 @@
-const baseURL =  process.env.REACT_APP_SERVER_URL;
+const baseURL =  process.env.REACT_APP_SERVER_URL_LOCAL;
 
 export const urls = {
-    categoriesEndpoint: `${baseURL}/api/Categories`,
-    subcategoriesEndpoint:`${baseURL}/api/Subcategories`,
-    teamsEndpoint: `${baseURL}/api/Teams`,
+    categoriesEndpoint: `${baseURL}api/Categories`,
+    subcategoriesEndpoint:`${baseURL}api/Subcategories`,
+    teamsEndpoint: `${baseURL}api/Teams`,
 }
