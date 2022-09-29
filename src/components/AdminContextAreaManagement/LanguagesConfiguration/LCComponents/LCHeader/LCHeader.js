00af572ccb57ac8c8f9216647797c73bbef18c33
markoparfeniuk <marko.parfeniuk.pm.2020@lpnu.ua>
2022-09-29 10:57:23 +0300

    code cleanup

--- src/components/AdminContextAreaManagement/LanguagesConfiguration/LCComponents/LCHeader/LCHeader.js
+++ src/components/AdminContextAreaManagement/LanguagesConfiguration/LCComponents/LCHeader/LCHeader.js
@@ -1,12 +1,9 @@
 import "./LCHeader.scss"
-import {useContext} from "react";
-import {Context} from "../../../../ContextProvider/ContextProvider";
 import AddLanguageModal from "../AddLanguageModal/AddLanguageModal";
 import { AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
 export default function LCHeader(){
-    const {NewLanguageButtonClicked, setNewLanguageButtonClicked} = useContext(Context);
     const [ modalOpen, setModalOpen ] = useState(false);
     const closeModal = () => setModalOpen(false);
     const openModal = () => setModalOpen(true);
